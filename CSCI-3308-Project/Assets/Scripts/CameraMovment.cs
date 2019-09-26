3ab5a0e300814f18ba5ca9526153de63aec50823
Patryk Wisniewski <PatrykJWisniewski@gmail.com>
2019-09-25 21:49:14 -0600

    Platformer Prototype
    Started working on the platformer prototype as a way to show the group what an example of what could be done.

---
+++ CSCI-3308-Project/Assets/Scripts/CameraMovment.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovment : MonoBehaviour
+{
+    public static CameraMovment S;
+
+    public GameObject[] arrayOfBackgrounds;
+    public GameObject[] arrayOfForegrounds;
+    public GameObject[] arrayOfGround;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        S = this;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //Moves the ground tiles
+        foreach (GameObject g in arrayOfGround)
+        {
+            Vector3 groundPos = g.transform.position;
+            groundPos.x += -.015f;
+            g.transform.position = groundPos;
+
+            //Moves the ground tiles too the right of the camera if they have gone too far too the lefgt
+            if (g.transform.position.x <= -10)
+            {
+                groundPos.x = 10;
+                g.transform.position = groundPos;
+            }
+        }
+
+        foreach (GameObject f in arrayOfForegrounds)
+        {
+            Vector3 forePos = f.transform.position;
+            forePos.x += -.01f;
+            f.transform.position = forePos;
+
+            if (f.transform.position.x <= -32)
+            {
+                forePos.x = 16;
+                f.transform.position = forePos;
+            }
+        }
+
+        foreach (GameObject b in arrayOfBackgrounds)
+        {
+            Vector3 backPos = b.transform.position;
+            backPos.x += -.005f;
+            b.transform.position = backPos;
+
+            if(b.transform.position.x <= -32)
+            {
+                backPos.x = 16;
+                b.transform.position = backPos;
+            }
+        }
+    }
+}
---
+++ CSCI-3308-Project/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            if (null != animator)
+            {
+                animator.CrossFade("Moving Forward", 1);
+                transform.eulerAngles = 0 * Vector3.up;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            if (null != animator)
+            {
+                animator.CrossFade("Moving Forward", 1);
+                transform.eulerAngles = 180 * Vector3.up;
+            }
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            if (null != animator)
+            {
+                transform.position += new Vector3(.05f, 0, 0);
+            }
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            if (null != animator)
+            {
+                transform.position += new Vector3(-.05f, 0, 0);
+            }
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            if (null != animator)
+            {
+                transform.position += new Vector3(0, -.05f, 0);
+            }
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            if (null != animator)
+            {
+                transform.position += new Vector3(0, .05f, 0);
+            }
+        }
+
+        if (Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.A))
+        {
+            if (null != animator)
+            {
+                animator.CrossFade("Idle", 1);
+            }
+        }
+    }
+}
