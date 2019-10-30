90726789319dc7b8d2c356bd6ddb0e8794a3919e
FordSuffers <jafo0664@colorado.edu>
2019-10-29 18:32:42 -0600

    This 10/29 Changes

---
+++ CSCI-3308-Project/Assets/Scripts/PlatformMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Vector3 posA;
+    private Vector3 posB;
+    private Vector3 nexPos;
+    [SerializeField]
+    private float speed;
+    void Start()
+    {
+        nexPos = posB;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
