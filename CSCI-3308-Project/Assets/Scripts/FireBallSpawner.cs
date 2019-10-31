fe1dabe2d47d7be2c9aab729f9d3932122ae1342
Patryk Wisniewski <PatrykJWisniewski@gmail.com>
2019-10-30 23:55:11 -0600

    Added fireballs to hidden boss fight
    Made it so fireballs spawn randomly and rain down on the platformer.

---
+++ CSCI-3308-Project/Assets/Scripts/FireBallSpawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBallSpawner : MonoBehaviour
+{
+    public GameObject fireBall;
+
+    private void Start()
+    {
+        InvokeRepeating("SpawnFireBall", 1, 2);
+    }
+
+    private void SpawnFireBall()
+    {
+        Vector3 velocity = new Vector3(Random.Range(2, 20), Random.Range(-10, -20));
+
+        GameObject go = Instantiate(fireBall);
+        go.transform.position = transform.position;
+        go.GetComponent<Rigidbody2D>().velocity = velocity;
+        go.transform.rotation = Quaternion.LookRotation(-velocity);
+
+        Destroy(go, 15);
+    }
+}
---
+++ CSCI-3308-Project/Assets/Scripts/Fireball.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fireball : MonoBehaviour
+{
+    private ParticleSystem ps;
+    public ParticleSystem childPS;
+
+    void Start()
+    {
+        ps = GetComponent<ParticleSystem>();
+    }
+
+    private void OnParticleTrigger()
+    {
+        
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        Debug.Log("coll");
+        bool moduleEnabled = false;
+        var emission = ps.emission;
+        var childEmission = childPS.emission;
+        emission.enabled = moduleEnabled;
+        childEmission.enabled = moduleEnabled;
+        var collision = ps.collision;
+        collision.enabled = moduleEnabled;
+    }
+}
