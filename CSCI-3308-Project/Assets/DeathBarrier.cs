666a4762fa1cc6dae1acf38abb0fdd3bc73015ed
FordSuffers <jafo0664@colorado.edu>
2019-11-13 17:23:44 -0700

    Death Barrier added

---
+++ CSCI-3308-Project/Assets/DeathBarrier.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathBarrier : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            LayerTrigger(collision);
+        }
+    }
+
+    void LayerTrigger(Collider2D player)
+    {
+        player_health stats = player.GetComponent<player_health>();
+        stats.health -= 1000;
+    }
+}
