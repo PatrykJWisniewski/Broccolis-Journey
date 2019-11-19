0c374df8bd2bfca5def575b4fc44d5a6db71653d
alexgill321 <54919681+alexgill321@users.noreply.github.com>
2019-11-18 17:16:39 -0700

    Player Damage and minor changes

--- CSCI-3308-Project/Assets/Scripts/player_health.cs
+++ CSCI-3308-Project/Assets/Scripts/player_health.cs
@@ -9,10 +9,26 @@ public class player_health : MonoBehaviour
     public float originalHealth = 2000f;
     public GameObject deathEffect;
     public GameObject FailLevelUI;
+    public Animator animator;
+    private float t1;
+    public Rigidbody2D m_Rigidbody2D;
+    private float invul = 0f;
+    public float recovery_time = .5f;
+    public CharacterController2D char_con;
 
     public void TakeDamage(int damage)
     {
-        health -= damage;
+       
+        if (!char_con.invulnerable)
+        {
+            m_Rigidbody2D.AddForce(new Vector2(-500 - (m_Rigidbody2D.velocity.x / Time.fixedDeltaTime), 700 - (m_Rigidbody2D.velocity.y / Time.fixedDeltaTime)));
+            animator.SetBool("isHurt", true);
+            char_con.hurt = true;
+            char_con.invulnerable = true;
+            invul = Time.time + .35f;
+            health -= damage;
+        }
+
         if (health <= 0f)
         {
             Die();
@@ -21,9 +37,19 @@ public class player_health : MonoBehaviour
 
     private void Update()
     {
-        if(health <= 0f)
+        if (char_con.invulnerable && Time.time > invul + recovery_time)
         {
-            Die();
+            char_con.invulnerable = false;
+            animator.SetBool("isHurt", false);
+        }
+        else if (char_con.hurt && Time.time > invul)
+        {
+            char_con.hurt = false;
+            animator.SetBool("isHurt", false);
+        }
+        if (health <= 0f)
+        {
+           Die();
         }
     }
     public void Die()
@@ -37,5 +63,4 @@ public class player_health : MonoBehaviour
         yield return new WaitForSeconds(4);
         SceneManager.LoadScene(1);
     }
-
 }
