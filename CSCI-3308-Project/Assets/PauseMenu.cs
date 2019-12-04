f343b547208ba1ca15a4e0dc2b4c66340d46786f
FordSuffers <jafo0664@colorado.edu>
2019-12-03 18:14:22 -0700

    Hub Stuff

--- CSCI-3308-Project/Assets/PauseMenu.cs
+++ CSCI-3308-Project/Assets/PauseMenu.cs
@@ -38,7 +38,8 @@ public class PauseMenu : MonoBehaviour
 
     public void LoadMenu()
     {
-
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(0);
     }
     public void QuitGame()
     {
